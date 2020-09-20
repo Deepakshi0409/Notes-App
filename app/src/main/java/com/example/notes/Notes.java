907af1b2090e3deb8a30492e754decf829a6c5a5
user <Deepakshi.deeps@gmail.com>
2020-09-20 10:19:51 +0530

    Initial commit: notes app

---
+++ app/src/main/java/com/example/notes/Notes.java
@@ -0,0 +1,47 @@
+package com.example.notes;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "notes")
+public class Notes {
+
+@PrimaryKey(autoGenerate = true)
+    private long id;
+@ColumnInfo(name ="Title")
+    private String title;
+
+@ColumnInfo(name = "Content")
+    private String content;
+
+public Notes(long id, String title, String content) {
+   this.id=id;
+   this.title = title;
+   this.content=content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
