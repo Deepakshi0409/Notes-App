fec2b98a0f3d559abe48c156e18cdc7c153d0c1f
user <Deepakshi.deeps@gmail.com>
2020-09-27 11:02:51 +0530

    Initial commit: notes app

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    private listviewModel viewModel;
+    private Notes notes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton addbtn = findViewById(R.id.add);
+        addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,addActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
