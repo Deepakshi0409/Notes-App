7242d7ed381ee54b00bfb9f8f24fff6cbc9b3f6c
Deepakshi0409 <Deepakshi.deeps@gmail.com>
2020-09-29 10:13:48 +0530

    Initial commit: notes app

--- app/src/main/java/com/example/notes/addActivity.java
+++ app/src/main/java/com/example/notes/addActivity.java
@@ -1,4 +1,22 @@
 package com.example.notes;
 
-public class addActivity {
+import android.view.View;
+import android.widget.AdapterView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class addActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    public static final String EXTRA_DATA_NAME = "extra_notes_name";
+    public static final String EXTRA_DATA_CONTENT = "extra_notes_content";
+    private addviewModel maddviewModel;
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
