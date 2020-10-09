43edfb0f2a410e2811a63ee182ed7ad264b9acc5
Deepakshi0409 <Deepakshi.deeps@gmail.com>
2020-10-09 13:43:02 +0530

    Updated dependency for FAB

--- app/src/main/java/com/example/notes/NotesDatabase.java
+++ app/src/main/java/com/example/notes/NotesDatabase.java
@@ -1,6 +1,8 @@
 package com.example.notes;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+
 
 import androidx.annotation.NonNull;
 import androidx.room.Database;
@@ -8,6 +10,14 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -26,6 +36,12 @@ public abstract class NotesDatabase extends RoomDatabase {
                         @Override
                         public void onCreate(@NonNull SupportSQLiteDatabase db) {
                             super.onCreate(db);
+                            executor.execute(new Runnable() {
+                                @Override
+                                public void run() {
+                                prepopulateDB(context.getAssets(),INSTANCE.notesDao());
+                                }
+                            });
                         }
                     }).fallbackToDestructiveMigration().build();
                 }
@@ -34,4 +50,43 @@ public abstract class NotesDatabase extends RoomDatabase {
         }
         return INSTANCE;
     }
+        private static void prepopulateDB(AssetManager assetManager, NotesDao notesDao){
+            BufferedReader bufferedReader = null;
+            StringBuilder stringBuilder = new StringBuilder();
+            String json = "";
+
+            try {
+                bufferedReader = new BufferedReader(new InputStreamReader(assetManager.open("myNotes.json")));
+                String mLine;
+                while ((mLine = bufferedReader.readLine())!=null){
+                    stringBuilder.append(mLine);
+                }
+                json=stringBuilder.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                if (bufferedReader!=null){
+                    try{
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            try {
+                JSONObject jsonObject = new JSONObject(json);
+                JSONArray Notes = jsonObject.getJSONArray("myNotes");
+                for (int i=0; i<Notes.length();i++){
+                    JSONObject myNotes = Notes.getJSONObject(i);
+                    String title = myNotes.getString("title");
+                    String content = myNotes.getString("content");
+                    Notes notes = new Notes(0L,title,content);
+                    notesDao.insertNotes(notes);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
 }
